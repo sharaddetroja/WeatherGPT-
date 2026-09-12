c3967d477aa3b3a9f1b44b462688beafa877b335
sharaddetroja <sharaddetroja@gmail.com>
2026-09-12 14:31:00 +0530

    feat: enable VITE_GOOGLE_CLIENT_ID environment variable support

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "435265044314-l1blt72lph5rcc6c54r0unmqudpfcipm.apps.googleusercontent.com";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GoogleOAuthProvider clientId="435265044314-l1blt72lph5rcc6c54r0unmqudpfcipm.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
         <App />
       </GoogleOAuthProvider>
     </BrowserRouter>
