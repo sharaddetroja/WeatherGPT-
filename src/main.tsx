2d423a38e90ce629b729f7f2f3d0ccde4e1e864f
aayusiagravat07 <aayusiagravat@gmail.com685>
2026-09-12 13:25:04 +0530

    feat: Add real Google OAuth login

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GoogleOAuthProvider clientId="435265044314-l1blt72lph5rcc6c54r0unmqudpfcipm.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
